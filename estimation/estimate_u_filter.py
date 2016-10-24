ebf4bee4a194b94bb54b9794c67642498f2e5675
hazhu <hazhu@nokia.com>
2016-10-25 07:56:20 +0800

    refactor estimate script

--- estimation/estimate_u_filter.py
+++ estimation/estimate_u_filter.py
@@ -3,10 +3,25 @@ sys.path.append("..\download")
 import get_stock_data_from_storage as sd
 sys.path.append("..\\analysis")
 import u_filter as uf
+import pandas as pd
+
+
+def get_n_days_ago_stock_storage(stock_storage, n):
+    ret = {}
+    for each_stock in stock_storage:
+        if len(stock_storage[each_stock]) > n:
+            ret[each_stock] = pd.DataFrame(stock_storage[each_stock].iloc[:-n, :])
+    return ret
 
 
 def estimate():
-    uf.search_all_stocks_with_u_shape(sd.CSVFile().load_all_history_stocks('../download/storage/'), 65, 15, True)
+    stock_storage = sd.CSVFile().load_all_history_stocks('../download/storage/')
+    his_storage = stock_storage
+    for i in range(100):
+        his_storage = get_n_days_ago_stock_storage(his_storage, 3)
+        filtered_stocks = uf.search_all_stocks_with_u_shape(his_storage, 65, 15, False)
+        print i, ':', filtered_stocks
 
 
-estimate()
+if __name__ == "__main__":
+    estimate()
